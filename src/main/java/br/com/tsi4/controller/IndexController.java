324da97c11e98c752de7d89a60884c9dab70f2c0
Pompeu <itacir@hotmail.com>
2014-10-22 16:34:53 -0200

    Vraptor Maven bootStrap On

---
+++ src/main/java/br/com/tsi4/controller/IndexController.java
@@ -0,0 +1,20 @@
+package br.com.tsi4.controller;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Result;
+
+@Controller
+public class IndexController {
+	@Inject private Result result;
+	@Path("/")
+	public void index(){
+		result.include("wecan", "Nos Podemos");
+		result.include("data", new Date(Calendar.getInstance().getTimeInMillis()));
+	}
+}
