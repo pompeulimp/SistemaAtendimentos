7d3d38b7bdc0688cfd65eb496019166f45e27177
Pompeu <itacir@hotmail.com>
2014-12-05 16:15:07 -0200

    fila prototipo on

--- src/main/java/br/com/tsi4/controller/FilaController.java
+++ src/main/java/br/com/tsi4/controller/FilaController.java
@@ -1,10 +1,82 @@
 package br.com.tsi4.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Result;
+import br.com.tsi4.model.Fila;
+import br.com.tsi4.model.DAO.FilaDAO;
+import br.com.tsi4.model.DAO.ICRUD;
 
 @Controller
 public class FilaController {
-	public void Fila() {
 
+	@Inject
+	private Result result;
+	private Fila fila;
+	private ICRUD<Fila> icrud;
+	private String mensagen = null;
+
+	public void Fila() {
+		icrud = new FilaDAO();
+	}
+
+	public void formulario() {
+	}
+
+	public void create(Fila fila) {
+
+		try {
+			if (fila.getpkFila() != 0) {
+				icrud.update(fila);
+				mensagen = "atualizado";
+			} else {
+				icrud.create(fila);
+				mensagen = "criado";
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		result.include("mensagem", "Tipo" + mensagen
+				+ " com sucesso!");
+		result.redirectTo(this).listar();
+	}
+
+	public List<Fila> listar() {
+		try {
+
+			return icrud.restriveAll();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void editar(long pkKey) {
+		try {
+			fila = icrud.retriveOneByPkKey(pkKey);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		result.include(fila);
+		result.of(this).formulario();
+	}
+
+	public void deletar(long pkKey) {
+		try {
+			if (icrud.delete(pkKey)) {
+				mensagen = "deletado";
+			} else {
+				mensagen = "não econtroado";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		result.include("mensagem", "Tipo" + mensagen);
+		result.redirectTo(this).listar();
 	}
 }
--- src/main/webapp/WEB-INF/jsp/hospital/formulario.jsp
+++ src/main/webapp/WEB-INF/jsp/hospital/formulario.jsp
@@ -22,7 +22,7 @@
  <form action="create">
  <fieldset>
  <legend>Cadastro de Hospitais</legend>
-  <input type="text" name="hospital.pkFila" value="${hospital.pkFila}" id="Fila" placeholder="Fila" class="form-control" size="50" >
+ <input type="text" name="hospital.pkFila" value="${hospital.pkFila}" id="Fila" placeholder="Fila" class="form-control" size="50" >
  <input type="text" name="hospital.razaosocial" value="${hospital.razaosocial}" id="razaosocial" placeholder="Razão social" class="form-control" size="50" >
  <input type="text" name="hospital.nomeFantasia" value="${hospital.nomeFantasia}" id="nomefantasia" placeholder="Nome fantasia" class="form-control" size="50" >
  <input type="text" name="hospital.cnpj" value="${hospital.cnpj}" id="cnpj" placeholder="CNPJ" class="form-control" size="50">
